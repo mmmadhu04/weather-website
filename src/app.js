d64733839c8b03dc379a5f57fc1e9664b0e3ecc9
Madhu M M <mmmadhu04.05@in.ibm.com>
2020-06-12 19:37:24 +0530

    Minor changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
 hbs.registerPartials(path.join(__dirname, '../templates/partials'))
@@ -86,6 +88,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000 !")
+app.listen(port, () => {
+    console.log("Server is up on port " + port + " !")
 })
